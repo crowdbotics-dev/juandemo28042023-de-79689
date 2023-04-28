d38bab822db1f226c294d9bbc06c1d88af59a3ff
Crowdbotics <engineering@crowdbotics.com>
2023-04-28 17:02:45 +0000

    Update store.

--- store/juandemodeAPI/api.js
+++ store/juandemodeAPI/api.js
@@ -3,6 +3,12 @@ const juandemodeAPI = axios.create({
   baseURL: "https://juandemo28042023-de-79689-prod.herokuapp.com",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function api_v1_ebank_list(payload) {
+  return juandemodeAPI.get(`/api/v1/ebank/`)
+}
+function api_v1_ebank_create(payload) {
+  return juandemodeAPI.post(`/api/v1/ebank/`, payload.data)
+}
 function api_v1_login_create(payload) {
   return juandemodeAPI.post(`/api/v1/login/`, payload.data)
 }
@@ -37,6 +43,18 @@ function rest_auth_logout_retrieve(payload) {
 }
 function rest_auth_logout_create(payload) {
   return juandemodeAPI.post(`/rest-auth/logout/`)
+}
+function api_v1_ebank_retrieve(payload) {
+  return juandemodeAPI.get(`/api/v1/ebank/${payload.id}/`)
+}
+function api_v1_ebank_update(payload) {
+  return juandemodeAPI.put(`/api/v1/ebank/${payload.id}/`, payload.data)
+}
+function api_v1_ebank_partial_update(payload) {
+  return juandemodeAPI.patch(`/api/v1/ebank/${payload.id}/`, payload.data)
+}
+function api_v1_ebank_destroy(payload) {
+  return juandemodeAPI.delete(`/api/v1/ebank/${payload.id}/`)
 }
 function api_v1_ewallet_retrieve(payload) {
   return juandemodeAPI.get(`/api/v1/ewallet/${payload.id}/`)
@@ -90,6 +108,8 @@ function rest_auth_registration_verify_email_create(payload) {
   )
 }
 export const apiService = {
+  api_v1_ebank_list,
+  api_v1_ebank_create,
   api_v1_login_create,
   api_v1_signup_create,
   api_v1_ewallet_list,
@@ -101,6 +121,10 @@ export const apiService = {
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
+  api_v1_ebank_retrieve,
+  api_v1_ebank_update,
+  api_v1_ebank_partial_update,
+  api_v1_ebank_destroy,
   api_v1_ewallet_retrieve,
   api_v1_ewallet_update,
   api_v1_ewallet_partial_update,
